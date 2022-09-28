cc2e540ba32c045849e18b442cf75640c878a070
pupil <pupil@ivkhk.ee>
2022-09-28 12:38:08 +0300

    first commit

---
+++ src/javahelllo/JavaHelllo.java
@@ -0,0 +1,16 @@
+
+package javahelllo;
+
+
+public class JavaHelllo {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("JavaHello!");
+    
+    
+    
+    }
+    
+}
